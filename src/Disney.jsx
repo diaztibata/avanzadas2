cda37e3aa7c5826fe71c7ca1c1c0be6dd33f5d25
Mikecito080 <134610551+Mikecito080@users.noreply.github.com>
2025-09-04 02:19:05 +0000

    Lopez_oferta

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+import Lopez_offers from './Lopez-oferta'
+
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <Lopez_offers/>
     </>
   )
 }
